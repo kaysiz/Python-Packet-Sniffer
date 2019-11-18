bcf20a205c151b48ee2ec71b32fbbfdcfb83c8b2
Kudakwashe Siziva <ksiziva@gmail.com>
2019-11-18 15:03:21 +0200

    Only listen to port 2055

--- sniffer.py
+++ sniffer.py
@@ -34,35 +34,34 @@ def main():
         # IPv4
         if eth.proto == 8:
             ipv4 = IPv4(eth.data)
-            print(TAB_1 + 'IPv4 Packet:')
-            print(TAB_2 + 'Version: {}, Header Length: {}, TTL: {},'.format(ipv4.version, ipv4.header_length, ipv4.ttl))
-            print(TAB_2 + 'Protocol: {}, Source: {}, Target: {}'.format(ipv4.proto, ipv4.src, ipv4.target))
-            print("heeeeeeeeeeeeeeeeeeeeeeeeeeeeeeell{}".format(ipv4.proto))
-            # ICMP
-            if ipv4.proto == 1:
-                icmp = ICMP(ipv4.data)
-                print(TAB_1 + 'ICMP Packet:')
-                print(TAB_2 + 'Type: {}, Code: {}, Checksum: {},'.format(icmp.type, icmp.code, icmp.checksum))
-                print(TAB_2 + 'ICMP Data:')
-                print(format_multi_line(DATA_TAB_3, icmp.data))
+            # print(TAB_1 + 'IPv4 Packet:')
+            # print(TAB_2 + 'Version: {}, Header Length: {}, TTL: {},'.format(ipv4.version, ipv4.header_length, ipv4.ttl))
+            # print(TAB_2 + 'Protocol: {}, Source: {}, Target: {}'.format(ipv4.proto, ipv4.src, ipv4.target))
+            # # ICMP
+            # if ipv4.proto == 1:
+            #     icmp = ICMP(ipv4.data)
+            #     print(TAB_1 + 'ICMP Packet:')
+            #     print(TAB_2 + 'Type: {}, Code: {}, Checksum: {},'.format(icmp.type, icmp.code, icmp.checksum))
+            #     print(TAB_2 + 'ICMP Data:')
+            #     print(format_multi_line(DATA_TAB_3, icmp.data))
 
-            # TCP
-            elif ipv4.proto == 6:
-                tcp = TCP(ipv4.data)
-                if tcp.dest_port == 2055:
-                    print(TAB_1 + 'TCP Segment:')
-                    print(TAB_2 + 'Source Port: {}, Destination Port: {}'.format(tcp.src_port, tcp.dest_port))
-                    print(TAB_2 + 'Sequence: {}, Acknowledgment: {}'.format(tcp.sequence, tcp.acknowledgment))
-                    print(TAB_2 + 'Flags:')
-                    print(TAB_3 + 'URG: {}, ACK: {}, PSH: {}'.format(tcp.flag_urg, tcp.flag_ack, tcp.flag_psh))
-                    print(TAB_3 + 'RST: {}, SYN: {}, FIN:{}'.format(tcp.flag_rst, tcp.flag_syn, tcp.flag_fin))
+            # # TCP
+            # elif ipv4.proto == 6:
+            #     tcp = TCP(ipv4.data)
+            #     if tcp.dest_port == 2055:
+            #         print(TAB_1 + 'TCP Segment:')
+            #         print(TAB_2 + 'Source Port: {}, Destination Port: {}'.format(tcp.src_port, tcp.dest_port))
+            #         print(TAB_2 + 'Sequence: {}, Acknowledgment: {}'.format(tcp.sequence, tcp.acknowledgment))
+            #         print(TAB_2 + 'Flags:')
+            #         print(TAB_3 + 'URG: {}, ACK: {}, PSH: {}'.format(tcp.flag_urg, tcp.flag_ack, tcp.flag_psh))
+            #         print(TAB_3 + 'RST: {}, SYN: {}, FIN:{}'.format(tcp.flag_rst, tcp.flag_syn, tcp.flag_fin))
 
-                    if len(tcp.data) > 0:
-                        print(TAB_2 + 'TCP Data:')
-                        print(format_multi_line(DATA_TAB_3, tcp.data))
+            #         if len(tcp.data) > 0:
+            #             print(TAB_2 + 'TCP Data:')
+            #             print(format_multi_line(DATA_TAB_3, tcp.data))
 
             # UDP
-            elif ipv4.proto == 17:
+            if ipv4.proto == 17:
                 udp = UDP(ipv4.data)
                 if udp.dest_port == 2055:
                     print(TAB_1 + 'UDP Segment:')
