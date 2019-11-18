e324b1ffadd06d3c10c95b148374c4f4ac643a26
Kudakwashe Siziva <ksiziva@gmail.com>
2019-11-18 16:22:36 +0200

    Only listen to port 2055

--- example.py
+++ example.py
@@ -1,6 +1,7 @@
 import socket
 import sys
 import binascii
+from struct import unpack
 
 host = ''
 port = 2055
@@ -16,9 +17,49 @@ s.listen(5)
 while True:
 
     conn, addr = s.accept()
-    data = conn.recvfrom(65565)
-    packet = data[0]
-    # reply = 'Server output: '+ str(binascii.hexlify(data))
-    print('packet: ' + str(packet))
-    print(reply)
-    print('connected to: ' +addr[0] + ':' + str(addr[1]))
+	packet = s.recvfrom(65565)
+
+	#packet string from tuple
+	packet = packet[0]
+
+	# take the first 20 characters of the ip header
+	ip_header = packet[0:20]
+
+	# now upack them
+	iph = unpack('!BBHHHBBH4s4s', ip_header)
+
+	version_ihl = iph[0]
+	version = version_ihl >> 4
+	ihl = version_ihl &  0xF
+
+	iph_length = ihl * 4
+
+	ttl = iph[5]
+	protocol = iph[6]
+	s_addr = socket.inet_ntoa(iph[8]);
+	d_addr = socket.inet_ntoa(iph[9]);
+
+	#print('Version : ' + str(version) + ' IP Header Length : ' + str(ihl) + ' TTL : ' + str(ttl) + ' Protocol : ' + str(protocol) + ' Source Address : ' + str(s_addr) + ' Destination Address : ' + str(d_addr))
+
+	tcp_header = packet[iph_length:iph_length+20]
+
+	#now unpack them :)
+	tcph = unpack('!HHLLBBHHH' , tcp_header)
+
+	source_port = tcph[0]
+	dest_port = tcph[1]
+	sequence = tcph[2]
+	acknowledgement = tcph[3]
+	doff_reserved = tcph[4]
+	tcph_length = doff_reserved >> 4
+    print('Version : ' + str(version) + ' IP Header Length : ' + str(ihl) + ' TTL : ' + str(ttl) + ' Protocol : ' + str(protocol) + ' Source Address : ' + str(s_addr) + ' Destination Address : ' + str(d_addr))
+    print('Source Port : ' + str(source_port) + ' Dest Port : ' + str(dest_port) + ' Sequence Number : ' + str(sequence) + ' Acknowledgement : ' + str(acknowledgement) + ' TCP header length : ' + str(tcph_length))
+
+    h_size = iph_length + tcph_length * 4
+    data_size = len(packet) - h_size
+
+    #get data from the packet
+    data = packet[h_size:]
+
+    #sys.stdout.buffer.write(data)
+    print('Data : ' + str(data))
