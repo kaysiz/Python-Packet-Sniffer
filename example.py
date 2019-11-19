e8931009e445c889f4d9b7eed509fc0b06546178
Kudakwashe Siziva <ksiziva@gmail.com>
2019-11-19 12:32:59 +0200

    Only listen to port 2055

--- example.py
+++ example.py
@@ -1,7 +1,117 @@
 import socket
 import sys
 import binascii
-from struct import unpack
+from struct import unpack, calcsize
+
+
+class Flow(object):
+	# Virtual base class
+	LENGTH = 0
+	def __init__(self, data):
+		if len(data) != self.LENGTH:
+			raise ValueError("Short flow")
+
+	def _int_to_ipv4(self, addr):
+		return "%d.%d.%d.%d" % \
+		   (addr >> 24 & 0xff, addr >> 16 & 0xff, \
+		    addr >> 8 & 0xff, addr & 0xff)
+
+class Header(object):
+	# Virtual base class
+	LENGTH = 0
+	def __init__(self, data):
+		if len(data) != self.LENGTH:
+			raise ValueError("Short flow header")
+
+class Header1(Header):
+	LENGTH = calcsize("!HHIII")
+	def __init__(self, data):
+		if len(data) != self.LENGTH:
+			raise ValueError("Short flow header")
+			
+		_nh = unpack("!HHIII", data)
+		self.version = _nh[0]
+		self.num_flows = _nh[1]
+		self.sys_uptime = _nh[2]
+		self.time_secs = _nh[3]
+		self.time_nsecs = _nh[4]
+
+	def __str__(self):
+		ret  = "NetFlow Header v.%d containing %d flows\n" % \
+		    (self.version, self.num_flows)
+		ret += "    Router uptime: %d\n" % self.sys_uptime
+		ret += "    Current time:  %d.%09d\n" % \
+		    (self.time_secs, self.time_nsecs)
+
+		return ret
+
+class Flow1(Flow):
+	LENGTH = calcsize("!IIIHHIIIIHHHBBBBBBI")
+	def __init__(self, data):
+		if len(data) != self.LENGTH:
+			raise ValueError("Short flow")
+			
+		_ff = unpack("!IIIHHIIIIHHHBBBBBBI", data)
+		self.src_addr = self._int_to_ipv4(_ff[0])
+		self.dst_addr = self._int_to_ipv4(_ff[1])
+		self.next_hop = self._int_to_ipv4(_ff[2])
+		self.in_index = _ff[3]
+		self.out_index = _ff[4]
+		self.packets = _ff[5]
+		self.octets = _ff[6]
+		self.start = _ff[7]
+		self.finish = _ff[8]
+		self.src_port = _ff[9]
+		self.dst_port = _ff[10]
+		# pad
+		self.protocol = _ff[12]
+		self.tos = _ff[13]
+		self.tcp_flags = _ff[14]
+
+	def __str__(self):
+		ret = "proto %d %s:%d > %s:%d %d bytes" % \
+		    (self.protocol, self.src_addr, self.src_port, \
+		     self.dst_addr, self.dst_port, self.octets)
+		return ret
+
+class NetFlowPacket:
+	FLOW_TYPES = {
+		1 : (Header1, Flow1),
+	}
+	def __init__(self, data):
+		if len(data) < 16:
+			raise ValueError("Short packet")
+		_nf = unpack("!H", data[:2])
+		self.version = _nf[0]
+
+		if not self.version in self.FLOW_TYPES.keys():
+			raise RuntimeWarning("NetFlow version %d is not yet implemented" % self.version)
+		hdr_class = self.FLOW_TYPES[self.version][0]
+		flow_class = self.FLOW_TYPES[self.version][1]
+
+		self.hdr = hdr_class(data[:hdr_class.LENGTH])
+
+		if len(data) - self.hdr.LENGTH != \
+		   (self.hdr.num_flows * flow_class.LENGTH):
+			raise ValueError("Packet truncated in flow data")
+		
+		self.flows = []
+		for n in range(self.hdr.num_flows):
+			offset = self.hdr.LENGTH + (flow_class.LENGTH * n)
+			flow_data = data[offset:offset + flow_class.LENGTH]
+			self.flows.append(flow_class(flow_data))
+
+	def __str__(self):
+		ret = str(self.hdr)
+		i = 0
+		for flow in self.flows:
+			ret += "Flow %d: " % i
+			ret += "%s\n" % str(flow)
+			i += 1
+
+		return ret
+
+
 
 host = ''
 port = 2055
@@ -17,7 +127,8 @@ s.listen(5)
 while True:
     
     conn, addr = s.accept()
-    packet = conn.recvfrom(65565)
+    packet, data = conn.recvfrom(65565)
+    print(NetFlowPacket(packet))
     # print(packet)
     # print(packet[:2][0])
     #packet string from tuple
--- test.py
+++ test.py
@@ -7,7 +7,7 @@ class Flow(object):
 	LENGTH = 0
 	def __init__(self, data):
 		if len(data) != self.LENGTH:
-			raise ValueError, "Short flow"
+			raise ValueError("Short flow")
 
 	def _int_to_ipv4(self, addr):
 		return "%d.%d.%d.%d" % \
@@ -19,13 +19,13 @@ class Header(object):
 	LENGTH = 0
 	def __init__(self, data):
 		if len(data) != self.LENGTH:
-			raise ValueError, "Short flow header"
+			raise ValueError("Short flow header")
 
 class Header1(Header):
 	LENGTH = struct.calcsize("!HHIII")
 	def __init__(self, data):
 		if len(data) != self.LENGTH:
-			raise ValueError, "Short flow header"
+			raise ValueError("Short flow header")
 			
 		_nh = struct.unpack("!HHIII", data)
 		self.version = _nh[0]
@@ -47,7 +47,7 @@ class Flow1(Flow):
 	LENGTH = struct.calcsize("!IIIHHIIIIHHHBBBBBBI")
 	def __init__(self, data):
 		if len(data) != self.LENGTH:
-			raise ValueError, "Short flow"
+			raise ValueError("Short flow")
 			
 		_ff = struct.unpack("!IIIHHIIIIHHHBBBBBBI", data)
 		self.src_addr = self._int_to_ipv4(_ff[0])
@@ -78,14 +78,12 @@ class NetFlowPacket:
 	}
 	def __init__(self, data):
 		if len(data) < 16:
-			raise ValueError, "Short packet"
+			raise ValueError("Short packet")
 		_nf = struct.unpack("!H", data[:2])
 		self.version = _nf[0]
 
 		if not self.version in self.FLOW_TYPES.keys():
-			raise RuntimeWarning, \
-			    "NetFlow version %d is not yet implemented" % \
-			    self.version
+			raise RuntimeWarning ("NetFlow version %d is not yet implemented" % self.version)
 		hdr_class = self.FLOW_TYPES[self.version][0]
 		flow_class = self.FLOW_TYPES[self.version][1]
 
@@ -93,7 +91,7 @@ class NetFlowPacket:
 
 		if len(data) - self.hdr.LENGTH != \
 		   (self.hdr.num_flows * flow_class.LENGTH):
-			raise ValueError, "Packet truncated in flow data"
+			raise ValueError("Packet truncated in flow data")
 		
 		self.flows = []
 		for n in range(self.hdr.num_flows):
@@ -111,7 +109,7 @@ class NetFlowPacket:
 
 		return ret
 
-host = None
+host = '127.0.0.1'
 port = 2055
 
 addrs = socket.getaddrinfo(host, port, socket.AF_UNSPEC, 
@@ -120,17 +118,17 @@ socks = []
 
 for addr in addrs:
 	sock = socket.socket(addr[0], addr[1])
-	sock.bind(('', 2055))
+	sock.bind(addr[4])
 	socks.append(sock)
 
-	print "listening on [%s]:%d" % (addr[4][0], addr[4][1])
+	print("listening on [%s]:%d" % (addr[4][0], addr[4][1]))
 
-while 1:
-	(rlist, wlist, xlist) = select.select(socks, [], socks)
-
-	for sock in rlist:
-		(data, addrport) = sock.recvfrom(8192)
-		print "Received flow packet from %s:%d" % addrport
-		print NetFlowPacket(data)
+while True:
+    (rlist, wlist, xlist) = select.select(socks, [], socks)
+    print("test")
+    for sock in rlist:
+        (data, addrport) = sock.recvfrom(8192)
+        print("Received flow packet from %s:%d" % addrport)
+		print(NetFlowPacket(data))
 
 
