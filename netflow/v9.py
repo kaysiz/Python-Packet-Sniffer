b84cf9698756e47cf66eff59736443687bc4c8d9
Kudakwashe Siziva <ksiziva@gmail.com>
2019-11-19 16:28:41 +0200

    Only listen to port 2055

--- netflow/v9.py
+++ netflow/v9.py
@@ -180,7 +180,8 @@ class DataFlowSet:
     """
     def __init__(self, data, templates):
         pack = struct.unpack('!HH', data[:4])
-
+        for x in pack:
+            print("shit " + x)
         self.template_id = pack[0]  # flowset_id is reference to a template_id
         print("erroring id : " + str(self.template_id))
         self.length = pack[1]
