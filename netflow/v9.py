89de3c10f6115e389d6d3057e92448149a756249
Kudakwashe Siziva <ksiziva@gmail.com>
2019-11-19 16:06:50 +0200

    Only listen to port 2055

--- netflow/v9.py
+++ netflow/v9.py
@@ -324,6 +324,7 @@ class V9ExportPacket:
         self.flows = []
 
         offset = self.header.length
+        print("Offset    " + str(offset))
         while offset != len(data):
             flowset_id = struct.unpack('!H', data[offset:offset+2])[0]
             print("hellow there    " + str(flowset_id))
