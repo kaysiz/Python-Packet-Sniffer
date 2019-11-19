11a54a810c422dcfc7ced43185760375f3634d58
Kudakwashe Siziva <ksiziva@gmail.com>
2019-11-19 12:07:22 +0200

    Only listen to port 2055

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ class Flow(object):
 	LENGTH = 0
 	def __init__(self, data):
 		if len(data) != self.LENGTH:
-			raise ValueError as "Short flow"
+			raise ValueError("Short flow")
 
 	def _int_to_ipv4(self, addr):
 		return "%d.%d.%d.%d" % \
@@ -19,13 +19,13 @@ class Header(object):
 	LENGTH = 0
 	def __init__(self, data):
 		if len(data) != self.LENGTH:
-			raise ValueError as "Short flow header"
+			raise ValueError("Short flow header")
 
 class Header1(Header):
 	LENGTH = struct.calcsize("!HHIII")
 	def __init__(self, data):
 		if len(data) != self.LENGTH:
-			raise ValueError as "Short flow header"
+			raise ValueError("Short flow header")
 			
 		_nh = struct.unpack("!HHIII", data)
 		self.version = _nh[0]
@@ -47,7 +47,7 @@ class Flow1(Flow):
 	LENGTH = struct.calcsize("!IIIHHIIIIHHHBBBBBBI")
 	def __init__(self, data):
 		if len(data) != self.LENGTH:
-			raise ValueError as "Short flow"
+			raise ValueError("Short flow")
 			
 		_ff = struct.unpack("!IIIHHIIIIHHHBBBBBBI", data)
 		self.src_addr = self._int_to_ipv4(_ff[0])
@@ -78,14 +78,12 @@ class NetFlowPacket:
 	}
 	def __init__(self, data):
 		if len(data) < 16:
-			raise ValueError as "Short packet"
+			raise ValueError("Short packet")
 		_nf = struct.unpack("!H", data[:2])
 		self.version = _nf[0]
 
 		if not self.version in self.FLOW_TYPES.keys():
-			raise RuntimeWarning as \
-			    "NetFlow version %d is not yet implemented" % \
-			    self.version
+			raise RuntimeWarning ("NetFlow version %d is not yet implemented" % self.version)
 		hdr_class = self.FLOW_TYPES[self.version][0]
 		flow_class = self.FLOW_TYPES[self.version][1]
 
@@ -93,7 +91,7 @@ class NetFlowPacket:
 
 		if len(data) - self.hdr.LENGTH != \
 		   (self.hdr.num_flows * flow_class.LENGTH):
-			raise ValueError, "Packet truncated in flow data"
+			raise ValueError("Packet truncated in flow data")
 		
 		self.flows = []
 		for n in range(self.hdr.num_flows):
