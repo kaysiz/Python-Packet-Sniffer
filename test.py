4c01005542c13c8acbb4a42a001323b58dfea275
Kudakwashe Siziva <ksiziva@gmail.com>
2019-11-19 12:09:00 +0200

    Only listen to port 2055

--- test.py
+++ test.py
@@ -123,9 +123,9 @@ for addr in addrs:
 
 	print("listening on [%s]:%d" % (addr[4][0], addr[4][1]))
 
-while 1:
+while True:
 	(rlist, wlist, xlist) = select.select(socks, [], socks)
-
+    print("test")
 	for sock in rlist:
 		(data, addrport) = sock.recvfrom(8192)
 		print("Received flow packet from %s:%d" % addrport)
