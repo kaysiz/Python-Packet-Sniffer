6aa7fc7c04d2e891dcaa5434cb755aacb7f17cd9
Kudakwashe Siziva <ksiziva@gmail.com>
2019-11-21 12:00:06 +0200

    Some changes

--- example.py
+++ example.py
@@ -154,7 +154,7 @@ while True:
     # if unpack("!H", packet[:2])[0] == 9:
     #     print(V9ExportPacket(packet, templates))
     
-    data = s.recv(1518)
+	data = s.recv(1518)
 	nfHeader = struct.unpack('!HHLLLL', data[0:20])
 	for flow in range(0, nfHeader[1]):
 		if flow == 0:
