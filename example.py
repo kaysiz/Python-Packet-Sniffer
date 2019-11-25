39fc9a58f97287b90c570175c6d2c400a5f804b2
Kudakwashe Siziva <ksiziva@gmail.com>
2019-11-25 12:18:56 +0200

    Updated code for meeting

--- example.py
+++ example.py
@@ -161,67 +161,67 @@ while True:
     #     print(V9ExportPacket(packet, templates))
     
 	data = conn.recv(1518)
-	get_netflow_version(data)
+	# get_netflow_version(data)
 	nfHeader = unpack('!HHLLLL', data[0:20])
 	version, count = unpack('!HH',data[0:4])
 	print("We have " + str(count) + " packets and Version is:  " + str(version))
-	V9ExportPacket(data, templates)
-	for flow in range(0, nfHeader[1]):
-		if flow == 0:
-			firstFlow = unpack('!IIIIIIIIBBHHBIBBBHH', data[24:74])
-			print("###################")
-			print(firstFlow)
-			print("###################")
-		else:
-			offset = flow * templSize
-			subseqFlow = unpack('!IIIIIIIIBBHHBIBBBHH', data[24 + offset:74 + offset])
-			print(subseqFlow)
-		print("Uptime is:  " + str(socket.ntohl(unpack('I',data[4:8])[0])))
-    # print("blows up")
-    # print(packet)
-    # print(packet[:2][0])
-    #packet string from tuple
-    # packet = packet[0]
+	# V9ExportPacket(data, templates)
+	# for flow in range(0, nfHeader[1]):
+	# 	if flow == 0:
+	# 		firstFlow = unpack('!IIIIIIIIBBHHBIBBBHH', data[24:74])
+	# 		print("###################")
+	# 		print(firstFlow)
+	# 		print("###################")
+	# 	else:
+	# 		offset = flow * templSize
+	# 		subseqFlow = unpack('!IIIIIIIIBBHHBIBBBHH', data[24 + offset:74 + offset])
+	# 		print(subseqFlow)
+	# 	print("Uptime is:  " + str(socket.ntohl(unpack('I',data[4:8])[0])))
+    # # print("blows up")
+    # # print(packet)
+    # # print(packet[:2][0])
+    # #packet string from tuple
+    # # packet = packet[0]
 
-    # # take the first 20 characters of the ip header
-    # ip_header = packet[0:20]
+    # # # take the first 20 characters of the ip header
+    # # ip_header = packet[0:20]
 
-    # # now upack them
-    # iph = unpack('!BBHHHBBH4s4s', ip_header)
-    # # print(unpack('!H', ip_header))
+    # # # now upack them
+    # # iph = unpack('!BBHHHBBH4s4s', ip_header)
+    # # # print(unpack('!H', ip_header))
 
-    # version_ihl = iph[0]
-    # version = version_ihl >> 4
-    # ihl = version_ihl &  0xF
+    # # version_ihl = iph[0]
+    # # version = version_ihl >> 4
+    # # ihl = version_ihl &  0xF
 
-    # iph_length = ihl * 4
+    # # iph_length = ihl * 4
 
-    # ttl = iph[5]
-    # protocol = iph[6]
-    # s_addr = socket.inet_ntoa(iph[8]);
-    # d_addr = socket.inet_ntoa(iph[9]);
+    # # ttl = iph[5]
+    # # protocol = iph[6]
+    # # s_addr = socket.inet_ntoa(iph[8]);
+    # # d_addr = socket.inet_ntoa(iph[9]);
 
-    # print('Version : ' + str(version) + ' IP Header Length : ' + str(ihl) + ' TTL : ' + str(ttl) + ' Protocol : ' + str(protocol) + ' Source Address : ' + str(s_addr) + ' Destination Address : ' + str(d_addr))
-
-    # tcp_header = packet[iph_length:iph_length+20]
-
-    # #now unpack them :)
-    # tcph = unpack('!HHLLBBHHH' , tcp_header)
-
-    # source_port = tcph[0]
-    # dest_port = tcph[1]
-    # sequence = tcph[2]
-    # acknowledgement = tcph[3]
-    # doff_reserved = tcph[4]
-    # tcph_length = doff_reserved >> 4
     # # print('Version : ' + str(version) + ' IP Header Length : ' + str(ihl) + ' TTL : ' + str(ttl) + ' Protocol : ' + str(protocol) + ' Source Address : ' + str(s_addr) + ' Destination Address : ' + str(d_addr))
-    # # print('Source Port : ' + str(source_port) + ' Dest Port : ' + str(dest_port) + ' Sequence Number : ' + str(sequence) + ' Acknowledgement : ' + str(acknowledgement) + ' TCP header length : ' + str(tcph_length))
 
-    # h_size = iph_length + tcph_length * 4
-    # data_size = len(packet) - h_size
+    # # tcp_header = packet[iph_length:iph_length+20]
 
-    # #get data from the packet
-    # data = packet[h_size:]
+    # # #now unpack them :)
+    # # tcph = unpack('!HHLLBBHHH' , tcp_header)
 
-    # #sys.stdout.buffer.write(data)
-    # # print('Data : ' + str(data))
+    # # source_port = tcph[0]
+    # # dest_port = tcph[1]
+    # # sequence = tcph[2]
+    # # acknowledgement = tcph[3]
+    # # doff_reserved = tcph[4]
+    # # tcph_length = doff_reserved >> 4
+    # # # print('Version : ' + str(version) + ' IP Header Length : ' + str(ihl) + ' TTL : ' + str(ttl) + ' Protocol : ' + str(protocol) + ' Source Address : ' + str(s_addr) + ' Destination Address : ' + str(d_addr))
+    # # # print('Source Port : ' + str(source_port) + ' Dest Port : ' + str(dest_port) + ' Sequence Number : ' + str(sequence) + ' Acknowledgement : ' + str(acknowledgement) + ' TCP header length : ' + str(tcph_length))
+
+    # # h_size = iph_length + tcph_length * 4
+    # # data_size = len(packet) - h_size
+
+    # # #get data from the packet
+    # # data = packet[h_size:]
+
+    # # #sys.stdout.buffer.write(data)
+    # # # print('Data : ' + str(data))
