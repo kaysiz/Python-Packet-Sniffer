d8a1e2149013c12dbad8d337415162304b462d4b
Kudakwashe Siziva <ksiziva@gmail.com>
2019-11-26 15:24:20 +0200

    New image

--- example.py
+++ example.py
@@ -136,7 +136,7 @@ def store_template_flowset(data):
 host = '127.0.0.1'
 port = 2055
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 try:
     s.bind((host, port))
