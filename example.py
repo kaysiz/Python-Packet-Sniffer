6d99fe12fad864345b2b7dcdec67efd3b7135873
Kudakwashe Siziva <ksiziva@gmail.com>
2019-11-26 15:17:05 +0200

    New image

--- example.py
+++ example.py
@@ -133,7 +133,7 @@ def store_template_flowset(data):
         i += 1
     return template_fields_type_length, template_flowset_length, template_flowset_field_count, offset
 
-host = ''
+host = '127.0.0.1'
 port = 2055
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
