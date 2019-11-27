ac08f07c384c41406d0285d45ec42ca5631eb281
Kudakwashe Siziva <ksiziva@gmail.com>
2019-11-27 16:12:17 +0200

    New image

--- test.py
+++ test.py
@@ -1,13 +1,61 @@
-import socket
+import socket, sys
 from struct import unpack
-
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 2055       # The port used by the server
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+	s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+	s.bind(('127.0.0.1', 2055))
+except socket.error as msg:
+	print('Socket could not be created. Error code: '+ str(msg[0]) + 'Message ' + msg[1])
+	sys.exit()
 
 while True:
-	s.connect((HOST, PORT))
-	data = s.recv(1024)
-	version, count = unpack('!HH',data[0:4])
+	# packet = s.recvfrom(65565)
+	packet, data = conn.recvfrom(65565)
+	version, count = unpack('!HH',packet[0:4])
 	print("We have " + str(count) + " packets and Version is:  " + str(version))
+
+	# #packet string from tuple
+	# packet = packet[0]
+
+	# # take the first 20 characters of the ip header
+	# ip_header = packet[0:20]
+
+	# # now upack them
+	# iph = unpack('!BBHHHBBH4s4s', ip_header)
+
+	# version_ihl = iph[0]
+	# version = version_ihl >> 4
+	# ihl = version_ihl &  0xF
+
+	# iph_length = ihl * 4
+
+	# ttl = iph[5]
+	# protocol = iph[6]
+	# s_addr = socket.inet_ntoa(iph[8]);
+	# d_addr = socket.inet_ntoa(iph[9]);
+
+	# #print('Version : ' + str(version) + ' IP Header Length : ' + str(ihl) + ' TTL : ' + str(ttl) + ' Protocol : ' + str(protocol) + ' Source Address : ' + str(s_addr) + ' Destination Address : ' + str(d_addr))
+
+	# tcp_header = packet[iph_length:iph_length+20]
+
+	# #now unpack them :)
+	# tcph = unpack('!HHLLBBHHH' , tcp_header)
+
+	# source_port = tcph[0]
+	# dest_port = tcph[1]
+	# sequence = tcph[2]
+	# acknowledgement = tcph[3]
+	# doff_reserved = tcph[4]
+	# tcph_length = doff_reserved >> 4
+
+	# if dest_port == 2055:
+	# 	print('Version : ' + str(version) + ' IP Header Length : ' + str(ihl) + ' TTL : ' + str(ttl) + ' Protocol : ' + str(protocol) + ' Source Address : ' + str(s_addr) + ' Destination Address : ' + str(d_addr))
+	# 	print('Source Port : ' + str(source_port) + ' Dest Port : ' + str(dest_port) + ' Sequence Number : ' + str(sequence) + ' Acknowledgement : ' + str(acknowledgement) + ' TCP header length : ' + str(tcph_length))
+
+	# 	h_size = iph_length + tcph_length * 4
+	# 	data_size = len(packet) - h_size
+
+	# 	#get data from the packet
+	# 	data = packet[h_size:]
+
+	# 	#sys.stdout.buffer.write(data)
+	# 	print('Data : ' + str(data))
