fb8eebdf23f8b125d4d8ab4835441b169a647651
Kudakwashe Siziva <ksiziva@gmail.com>
2019-11-27 16:24:01 +0200

    New image

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from socket import inet_ntoa
 SIZE_OF_HEADER = 24
 SIZE_OF_RECORD = 48
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(('127.0.0.1', 2055))
 
 while True:
